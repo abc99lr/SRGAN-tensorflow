84cd1cf1deecbfc4f56a1b4bff6973990d63d347
Rui Lan <abc99lr@gmail.com>
2018-03-18 09:32:26 -0500

    testing srgan 100000

--- test_SRGAN.sh
+++ test_SRGAN.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=0 python main.py \
-    --output_dir /home/ruilan2/scratch/MPR0Result/test_SRResnet_80000/ \
-    --summary_dir /home/ruilan2/scratch/MPR0Result/test_SRResnet_80000/log/ \
+    --output_dir /home/ruilan2/scratch/MPR0Result/test_SRGAN_100000/ \
+    --summary_dir /home/ruilan2/scratch/MPR0Result/test_SRGAN_100000/log/ \
     --mode test \
     --is_training False \
-    --task SRResnet \
+    --task SRGAN \
     --batch_size 16 \
     --input_dir_LR /home/ruilan2/scratch/MPR0/MPR0_LR_test_small/ \
     --input_dir_HR /home/ruilan2/scratch/MPR0/MPR0_HR_test_small/ \
     --num_resblock 16 \
     --perceptual_mode MSE \
     --pre_trained_model True \
-    --checkpoint /home/ruilan2/scratch/MPR0Result/experiment_SRResnet/model-80000
+    --checkpoint /home/ruilan2/scratch/MPR0Result/experiment_SRGAN_MSE/model-100000
 
