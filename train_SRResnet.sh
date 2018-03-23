5364ff7f7c98658dee041eae5f59142b3ae9d762
Rui Lan <abc99lr@gmail.com>
2018-03-22 20:36:05 -0500

    srres

--- train_SRResnet.sh
+++ train_SRResnet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=0 python main.py \
-    --output_dir /home/ruilan2/scratch/MPR0Result/experiment_SRResnet/ \
-    --summary_dir /home/ruilan2/scratch/MPR0Result/experiment_SRResnet/log/ \
+    --output_dir /home/ruilan2/scratch/MPR0Result/experiment_SRResnet_scale4/ \
+    --summary_dir /home/ruilan2/scratch/MPR0Result/experiment_SRResnet_scale4/log/ \
     --mode train \
     --is_training True \
     --task SRResnet \
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --flip False \
     --random_crop True \
     --crop_size 24 \
-    --input_dir_LR /home/ruilan2/scratch/MPR0/MPR0_LR_train/ \
+    --input_dir_LR /home/ruilan2/scratch/MPR0/MPR0_LR_train_scale4/ \
     --input_dir_HR /home/ruilan2/scratch/MPR0/MPR0_HR_train/ \
     --num_resblock 16 \
     --name_queue_capacity 4096 \
